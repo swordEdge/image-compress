308539ae5fd2fff4d88ad51f38417027e2ffa7f0
hiroshi <robinhoodie0823@gmail.com>
2022-07-25 01:05:51 +0900

    create image compressor with next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.css";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+//   useEffect(() => {
+//     import("bootstrap/dist/js/bootstrap");
+// }, []);
+  return (
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
